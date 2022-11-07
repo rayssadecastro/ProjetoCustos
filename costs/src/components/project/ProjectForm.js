e721cc8c6c32d1b203192ff9fc2ca8f230e83695
Rayssa Vitória de Castro Gonçalves <79856364+rayssadecastro@users.noreply.github.com>
2022-11-07 08:16:03 -0300

    Add files via upload

---
+++ costs/src/components/project/ProjectForm.js
@@ -0,0 +1,24 @@
+import styles from './ProjectForm.module.css'
+import Input from '../form/input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
+
+function ProjectForm ({btnText}){
+    return(
+        <form className={styles.form}>
+            <Input type="text"
+             text="Nome do projeto" 
+             name="name" 
+             placeholder="Insira o nome do projeto"/>
+            <Input type="number"
+             text="Orçamento do projeto" 
+             name="budget" 
+             placeholder="Insira o orçamento total"/>
+            <Select name="category_id" text="Selecione uma categoria"/>
+            <SubmitButton text={btnText} />
+            
+        </form>
+    )
+}
+
+export default ProjectForm
